51d1057c734dd22b089ea12e9be2f77a558fca03
Tucker Connelly <web@tuckerconnelly.com>
2016-05-12 06:57:31 -0400

    Switch to react-native-universal

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 const merge = require('lodash').merge
 
-var React
-
-// HACK Differentiates import depending on if we're running in server,
-// web client, or react native
-// See tuckerconnelly/carbon-ui/src/React.js
-if (process && !global.__BUNDLE_START_TIME__) React = require('react-native-web')
-else React = require('react-native')
+const ReactNative = require('react-native-universal')
 
 module.exports = function (styles) {
-  switch (React.Platform.OS) {
+  switch (ReactNative.Platform.OS) {
     case 'ios':
       return merge(styles, styles.ios || {})
     case 'android':
