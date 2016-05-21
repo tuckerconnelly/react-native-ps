1b2ec89ec6485f2f0c3d971562625c3246b7e0b1
Tucker Connelly <web@tuckerconnelly.com>
2016-05-21 00:35:06 -0400

    Add support for functions

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const merge = require('lodash').merge
+const lodash = require('lodash')
+const merge = lodash.merge
+const mergeWith = lodash.mergeWith
 
 const ReactNative = require('react-native-universal')
 
-module.exports = function (styles) {
+const evaluate = (func, args) => {
+  if (typeof func !== 'function') return func
+  return func.apply(null, args)
+}
+
+// Handle functions
+const customizer = (objValue, srcValue) => {
+  if (typeof objValue === 'function' || typeof srcValue === 'function') {
+    return function () {
+      return merge(evaluate(objValue, arguments), evaluate(srcValue, arguments))
+    }
+  }
+}
+
+module.exports = styles => {
   switch (ReactNative.Platform.OS) {
     case 'ios':
-      return merge(styles, styles.ios || {})
+      return mergeWith(styles, styles.ios || {}, customizer)
     case 'android':
-      return merge(styles, styles.android || {})
+      return mergeWith(styles, styles.android || {}, customizer)
     case 'web':
-      return merge(styles, styles.web || {})
+      return mergeWith(styles, styles.web || {}, customizer)
   }
 }
